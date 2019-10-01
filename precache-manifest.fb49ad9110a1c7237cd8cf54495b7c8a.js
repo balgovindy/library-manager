b629ee528b5b6a0edf93ee1c31bb3ec79caf4bab
balgovindy <balgovindy@mitrmedia.com>
2019-10-01 16:00:17 +0530

    added

---
+++ precache-manifest.fb49ad9110a1c7237cd8cf54495b7c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0736f1fef0570cfd19ee8323017af0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3006a31f028c749b2293",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "0605d20cf1a1c597778a",
+    "url": "/static/css/main.db71855e.chunk.css"
+  },
+  {
+    "revision": "3006a31f028c749b2293",
+    "url": "/static/js/2.c02bc5ec.chunk.js"
+  },
+  {
+    "revision": "0605d20cf1a1c597778a",
+    "url": "/static/js/main.13f0af9b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
